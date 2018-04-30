530d66e60a971af2ca8215d7046cba6d6e750e8c
Neddy <6915421+neddy@users.noreply.github.com>
2018-04-30 18:44:22 +1000

    Pushing updates: Mon 30 Apr 2018 18:44:22 AEST

--- Exercises/101-109 Small Problems/10 Medium/ex_10.rb
+++ Exercises/101-109 Small Problems/10 Medium/ex_10.rb
@@ -14,13 +14,10 @@ def fibonacci_last(n)
   fibonacci(n).to_s[-1].to_i
 end
 
-
 p fibonacci_last(15)        # -> 0  (the 15th Fibonacci number is 610)
 p fibonacci_last(20)        # -> 5 (the 20th Fibonacci number is 6765)
 p fibonacci_last(100)       # -> 5 (the 100th Fibonacci number is 354224848179261915075)
 p fibonacci_last(100_001)   # -> 1 (this is a 20899 digit number)
-
-
 
 
 # Further Exploration
