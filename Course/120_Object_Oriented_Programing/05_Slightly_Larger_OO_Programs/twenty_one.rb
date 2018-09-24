fd9ce7655e328dfdb70070f414b2ffb6a3858292
Neddy <6915421+neddy@users.noreply.github.com>
2018-09-24 17:33:49 +1000

    Further refactor Twenty-One Game

--- Course/120_Object_Oriented_Programing/05_Slightly_Larger_OO_Programs/twenty_one.rb
+++ Course/120_Object_Oriented_Programing/05_Slightly_Larger_OO_Programs/twenty_one.rb
@@ -1,30 +1,22 @@
 module GameHelper
   WIDTH = 48
 
-  def output(*msg)
-    case msg.size
-    when 1 then output_center(msg)
-    when 2 then output_left_right(msg)
-    when 3 then output_left_center_right(msg)
-    end
-  end
-
   def output_center(msg)
-    puts msg[0].center(WIDTH)
+    puts msg.center(WIDTH)
   end
 
-  def output_left_right(msg)
-    puts msg[0].ljust(WIDTH / 2) + msg[1].rjust(WIDTH / 2)
+  def output_left_right(left, right)
+    puts left.ljust(WIDTH / 2) + right.rjust(WIDTH / 2)
   end
 
-  def output_left_center_right(msg)
-    puts msg[0].ljust(WIDTH / 4) +
-         msg[1].center(WIDTH / 2) +
-         msg[2].rjust(WIDTH / 4)
+  def output_left_center_right(left, center, right)
+    puts left.ljust(WIDTH / 4) +
+         center.center(WIDTH / 2) +
+         right.rjust(WIDTH / 4)
   end
 
   def display_heading(heading)
-    output(heading)
+    output_center(heading)
     output_blank_line
   end
 
@@ -33,7 +25,7 @@ module GameHelper
   end
 
   def invalid_input
-    output('Invalid input, please try again.')
+    output_center('Invalid input, please try again.')
   end
 
   def output_blank_line
@@ -46,12 +38,12 @@ module GameHelper
 
   def press_return_to_continue
     output_blank_line
-    output("Press return to continue...")
+    output_center("Press return to continue...")
     output_blank_line
   end
 
   def y_and_n?
-    output("Please enter y or n.")
+    output_center("Please enter y or n.")
     choice = ''
     loop do
       input_prompt
@@ -69,30 +61,24 @@ class Hand
   end
 
   def display
-    cards_array = create_cards_output_array
-    output_cards(cards_array)
+    cards_output_array = create_cards_output_array
+    output_cards(cards_output_array)
   end
 
-  def display_hide_card
-    cards_array = create_cards_output_array
-    hide_second_card!(cards_array)
-    output_cards(cards_array)
+  def display_with_second_card_hidden
+    cards_output_array = create_cards_output_array
+    hide_second_card!(cards_output_array)
+    output_cards(cards_output_array)
   end
 
   def value
     total = 0
     aces = 0
-    aces_reduced_to_one = 0
     cards.each do |card|
       total += card.value
       aces += 1 if card.rank == 'A'
     end
-    loop do
-      break if total <= 21 || aces == aces_reduced_to_one
-      total -= 10
-      aces_reduced_to_one += 1
-    end
-    total
+    adjust_total_for_aces(total, aces)
   end
 
   def <<(card)
@@ -102,6 +88,16 @@ class Hand
   private
 
   attr_accessor :cards
+
+  def adjust_total_for_aces(total, aces)
+    aces_reduced_to_one = 0
+    loop do
+      break if total <= 21 || aces == aces_reduced_to_one
+      total -= 10
+      aces_reduced_to_one += 1
+    end
+    total
+  end
 
   def output_cards(cards_array)
     7.times do |idx|
@@ -194,7 +190,7 @@ class Player < Participant
 
   def set_name
     name = ''
-    output('Please enter your name:')
+    output_center('Please enter your name:')
     loop do
       input_prompt
       name = gets.chomp.strip
@@ -206,7 +202,7 @@ class Player < Participant
   end
 
   def retrieve_player_choice
-    output("(H)it or (S)tay?")
+    output_center("(H)it or (S)tay?")
     choice = ''
     loop do
       input_prompt
@@ -221,22 +217,22 @@ end
 class Dealer < Participant
   def reset
     super
-    self.hide_card = true
+    self.hide_second_card = true
   end
 
   def take_turn(deck)
     if hand.value >= 17
-      output("#{name} stays.")
+      output_center("#{name} stays.")
       self.stay = true
     else
-      output("#{name} hitting...")
+      output_center("#{name} hitting...")
       draw_card(deck)
     end
     sleep(1)
   end
 
-  def toggle_hide_card
-    self.hide_card = false
+  def unhide_second_card
+    self.hide_second_card = false
   end
 
   def stay?
@@ -244,8 +240,8 @@ class Dealer < Participant
   end
 
   def display_hand
-    if hide_card?
-      hand.display_hide_card
+    if hide_second_card?
+      hand.display_with_second_card_hidden
     else
       super
     end
@@ -253,14 +249,14 @@ class Dealer < Participant
 
   private
 
-  attr_accessor :hide_card
+  attr_accessor :hide_second_card
 
   def set_name
     self.name = %w[Chappie Hal R2D2 C-3PO].sample
   end
 
-  def hide_card?
-    hide_card
+  def hide_second_card?
+    hide_second_card
   end
 end
 
@@ -339,7 +335,7 @@ class TwentyOneGame
       display_play_again
       reset_game
     end
-    output("Thanks for playing, good bye!")
+    output_center("Thanks for playing, good bye!")
   end
 
   private
@@ -351,7 +347,7 @@ class TwentyOneGame
     deal_cards
     display_hands
     take_turn(player)
-    dealer.toggle_hide_card
+    dealer.unhide_second_card
     display_hands
     unless player.busted?
       take_turn(dealer)
@@ -368,7 +364,7 @@ class TwentyOneGame
 
   def play_again?
     output_blank_line
-    output("Would you like to play again?")
+    output_center("Would you like to play again?")
     y_and_n?
   end
 
@@ -404,10 +400,10 @@ class TwentyOneGame
   def display_hands
     clear_screen
     display_heading(HEADING)
-    output("#{dealer.name}'s Hand:", "")
+    output_left_right("#{dealer.name}'s Hand:", "")
     dealer.display_hand
     output_blank_line
-    output("#{player.name}'s Hand:", "")
+    output_left_right("#{player.name}'s Hand:", "")
     player.display_hand
   end
 
@@ -415,10 +411,10 @@ class TwentyOneGame
     result = determine_result
     output_blank_line
     case result
-    when :player_busted then display_player_busted
-    when :dealer_busted then display_dealer_busted
-    when :player_won then display_player_won
-    when :dealer_won then display_dealer_won
+    when :player_busted then display_busted(player)
+    when :dealer_busted then display_busted(dealer)
+    when :player_won then display_won(player)
+    when :dealer_won then display_won(dealer)
     when :tie then display_tie
     end
   end
@@ -427,42 +423,35 @@ class TwentyOneGame
     output_blank_line
     player_total = "#{player.name}'s hand: #{player.hand_value}"
     dealer_total = "#{dealer.name}'s hand: #{dealer.hand_value}"
-    output(player_total, dealer_total)
+    output_left_right(player_total, dealer_total)
   end
 
   def display_welcome
     display_heading(HEADING)
-    output("Welcome to Twenty One #{player.name}")
+    output_center("Welcome to Twenty One #{player.name}")
     output_blank_line
-    output("Your dealer will be #{dealer.name}")
+    output_center("Your dealer will be #{dealer.name}")
     press_return_to_continue
   end
 
   def display_play_again
     clear_screen
     display_heading(HEADING)
-    output("Great, let's play again!")
+    output_center("Great, let's play again!")
     press_return_to_continue
   end
 
-  def display_player_busted
-    output("#{player.name} busted! #{dealer.name} wins!")
+  def display_busted(participant)
+    winner = participant == player ? dealer : player
+    output_center("#{participant.name} busted! #{winner.name} wins!")
   end
 
-  def display_dealer_busted
-    output("#{dealer.name} busted! #{player.name} wins!")
-  end
-
-  def display_player_won
-    output("#{player.name} won")
-  end
-
-  def display_dealer_won
-    output("#{dealer.name} won!")
+  def display_won(participant)
+    output_center("#{participant.name} won!")
   end
 
   def display_tie
-    output("It's a tie")
+    output_center("It's a tie")
   end
 end
 
