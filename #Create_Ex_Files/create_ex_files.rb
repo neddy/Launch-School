749ccfed24dec4fa3043b0b4ffc819ed8ecab6a6
Neddy <6915421+neddy@users.noreply.github.com>
2019-04-18 08:40:50 +1000

    Update script to create exercise files to use constant

--- #Create_Ex_Files/create_ex_files.rb
+++ #Create_Ex_Files/create_ex_files.rb
@@ -1,5 +1,6 @@
-exercises = 10
-(1..exercises).each do |n|
+EXERCISES = 10
+
+(1..EXERCISES).each do |n|
   filename = "ex_#{n}.rb"
   File.open(filename, "w") { |file| file.write("# #{filename}\n# ") }
 end
