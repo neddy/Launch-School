16280e971e5ff461295dd33f13df9398ae9eb67f
Neddy <6915421+neddy@users.noreply.github.com>
2019-04-18 08:43:30 +1000

    Create new script to create exercise files for 'testing' exercises

---
+++ #Create_Ex_Files/create_ex_files_for_testing.rb
@@ -0,0 +1,18 @@
+EXERCISES = 1
+
+(1..EXERCISES).each do |n|
+  filename = "ex_#{n}.rb"
+  file_contents = <<-FILECONTENTS
+require 'minitest/autorun'
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+class Testing < MiniTest::Test
+  def test_
+
+  end
+end
+  FILECONTENTS
+  file_contents.prepend("# #{filename}\n# \n\n")
+  File.open(filename, "w") { |file| file.write(file_contents) }
+end
